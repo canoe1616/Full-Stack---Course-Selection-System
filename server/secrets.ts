e5d92761e30dd117908f03ff4833339bafff47c6
Ian Liu <cl583@duke.edu>
2022-12-02 17:45:53 +0000

    Test

--- server/secrets.ts
+++ server/secrets.ts
@@ -1,6 +1,6 @@
 export const keycloak = {
   client_id: "course-registration",
-  client_secret: "yUkDvdkJJZoJtJGmrHBqxRy6ryLFBpMB",
+  client_secret: "QHcQ0IDo7Ff3S6lp8JqvXPNq9s3DbZsD",
   redirect_uris: ["http://127.0.0.1:8096/api/login-callback"],
   post_logout_redirect_uris: [""],
   response_types: ["code"],
